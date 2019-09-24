e7ceb0cd7de7e1b8c91fa49b99830061fede5ebc
Qinxiang Zhu <verios.zhu@gmail.com>
2019-09-24 23:00:16 +0800

    fix camera service error

--- app/src/main/java/zhu/verios/veriosdemo/camera/CameraActivity.kt
+++ app/src/main/java/zhu/verios/veriosdemo/camera/CameraActivity.kt
@@ -1,6 +1,9 @@
 package zhu.verios.veriosdemo.camera
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.hardware.Camera
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.view.View
@@ -28,8 +31,19 @@ class CameraActivity: AppCompatActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_camera)
         val cameraNumber = Camera.getNumberOfCameras()
         Log.d(TAG, "camera number: $cameraNumber")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(arrayOf(Manifest.permission.CAMERA), 1)
+            } else {
+                openCamera()
+            }
+        }
+    }
+
+    fun openCamera(){
         if (CameraUtils.checkCameraHardware(this)) {
             camera = CameraUtils.getCameraInstance()!!
             preview = CameraPreview(this, camera)
@@ -40,6 +54,17 @@ class CameraActivity: AppCompatActivity(), View.OnClickListener {
     override fun onClick(v: View) {
         when(v.id) {
             R.id.btn_capture -> camera.takePicture(null, null, pictureCallback)
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        when (requestCode) {
+            1 ->
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    //Start your camera handling here
+                    openCamera()
+                }
         }
     }
 
